41ab73ed757f11be201f27098f7acf7bc868bb09
Hanna Yurusova <Annabuynevich@rambler.ru>
2025-10-27 13:40:05 +0300

    added new code lines

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-protected $fillable = ['name', 'status', 'priority'];
+    protected $fillable = 
+    ['name', 
+    'status', 
+    'priority'
+];
 }
